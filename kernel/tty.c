07b771467b5226c9b805a88fc42311ec37346680
wyh <708949379@qq.com>
2019-11-21 17:21:54 +0800

    finished enter and backspace

--- kernel/tty.c
+++ kernel/tty.c
@@ -15,6 +15,7 @@
 PRIVATE void init_tty(TTY* p_tty);
 PRIVATE void tty_do_read(TTY* p_tty);
 PRIVATE void tty_do_write(TTY*p_tty);
+PRIVATE void put_key(TTY* p_tty, u32 key);
 
 PUBLIC void task_tty(){
 	TTY* p_tty;
@@ -64,26 +65,27 @@ PUBLIC void in_process(TTY* p_tty, u32 key)
         char output[2] = {'\0', '\0'};
 
         if (!(key & FLAG_EXT)) {
-		if (p_tty->inbuf_count < TTY_IN_BYTES) {
-			*(p_tty->p_inbuf_head) = key;
-			p_tty->p_inbuf_head++;
-			if (p_tty->p_inbuf_head == p_tty->in_buf + TTY_IN_BYTES) {
-				p_tty->p_inbuf_head = p_tty->in_buf;
-			}
-			p_tty->inbuf_count++;
-		}
+			put_key(p_tty, key);
         }
         else {
                 int raw_code = key & MASK_RAW;
                 switch(raw_code) {
+				case ENTER:
+					put_key(p_tty, '\n');
+					break;
+				case BACKSPACE:
+					put_key(p_tty, '\b');
+					break;
                 case UP:
-                        if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R))
-				scroll_screen(p_tty->p_console, SCR_DN);
-			break;
-		case DOWN:
-			if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R))
-				scroll_screen(p_tty->p_console, SCR_UP);
-			break;
+                    if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)){
+						scroll_screen(p_tty->p_console, SCR_DN);
+					}
+					break;
+				case DOWN:
+					if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)){
+						scroll_screen(p_tty->p_console, SCR_UP);
+					}
+					break;
 		case F1:
 		case F2:
 		case F3:
@@ -105,4 +107,15 @@ PUBLIC void in_process(TTY* p_tty, u32 key)
                         break;
                 }
         }
+}
+
+PRIVATE void put_key(TTY* p_tty, u32 key){
+	if(p_tty->inbuf_count < TTY_IN_BYTES){
+		*(p_tty->p_inbuf_head) = key;
+		p_tty->p_inbuf_head++;
+		if(p_tty->p_inbuf_head == p_tty->in_buf + TTY_IN_BYTES){
+			p_tty->p_inbuf_head = p_tty->in_buf;
+		}
+		p_tty->inbuf_count++;
+	}
 }
